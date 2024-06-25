4928c7e9d761e43d17b09d7c4de74d00b9ade4a0
sneufville <sneufville@academic.rrc.ca>
2024-06-25 15:37:46 -0500

    add model for service area

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_25_203159) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_25_203627) do
   create_table "electoral_wards", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "service_areas", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
